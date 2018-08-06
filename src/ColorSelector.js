03785e5773d5bc8512ccc855537b526163bc6f61
jchamberlain909 <jchamberlain909@gmail.com>
2018-08-06 10:46:23 -0400

    Done.

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -4,9 +4,13 @@ export default class ColorSelector extends Component {
   
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
+      return <div onClick={this.onClickHandler} key={idx} className="color-swatch" style={{backgroundColor: str}}/>
     })
   )
+
+  onClickHandler = (e)=>{
+    this.props.setSelectedColor(e.target.style.backgroundColor)
+  }
   
   render() {
     return (
